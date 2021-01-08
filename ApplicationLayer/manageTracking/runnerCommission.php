70c0beb25c1b08138d779774ab5ab0fa7ecc3757
Yan <yinchi16@gmail.com>
2021-01-08 21:11:22 +0800

    add button and report

--- ApplicationLayer/manageTracking/runnerCommission.php
+++ ApplicationLayer/manageTracking/runnerCommission.php
@@ -1,10 +1,91 @@
 <?php
     require_once '../../BusinessLayer/controller/manageTrackingController.php';
+    require '../manageTracking/fpdf182/fpdf.php';
 
     session_start();
 
     $notification = new manageTrackingController();
     $data = $notification->viewRunnerP();
+
+    
+
+    class printPDF extends FPDF{
+
+        function header(){
+            $this->Image('Image/logo64.jpg', 10,6);
+            $this->SetFont('Arial','I',12);
+            $this->Cell(70,10,'Beep Beep Delivery',0,0,'C');
+            $this->SetFont('Arial','B',16);
+            $this->Cell(150,7,'Commission Report',0,0,'C');
+            $this->Ln();
+        }
+        function body(){
+            $this->SetXY(120,30);
+            $this->SetFont('Arial','B',12, 'C');
+            $this->Cell(40,10,'Runner:',0,0,);
+            $this->SetFont('Arial','',12, 'C');
+            $this->Cell(150,10,$_SESSION['runnerusername'],0,0,);
+            $this->Ln();
+            $this->SetX(120);
+            $this->SetFont('Arial','B',12, 'C');
+            $this->Cell(40,10,'Runner ID:',0,0,);
+            $this->SetFont('Arial','',12, 'C');
+            $this->Cell(150,10,$_SESSION['runnerID'],0,0,);
+            $this->Ln();
+        }
+        function footer(){
+            $this->SetY(-15);
+            $this->SetFont('Arial','',8);
+            $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+            $this->SetTextColor(194,8,8);
+        }
+        function headerTable(){
+            $this->SetXY(35,60);
+            $this->SetFont('Times','B',12);
+            $this->Cell(30,10,'No',1,0,'C');
+            $this->Cell(50,10,'Item Name',1,0,'C');
+            $this->Cell(50,10,'Quantity',1,0,'C');
+            $this->Cell(50,10,'Delivery Fee (RM)',1,0,'C');
+            $this->Cell(50,10,'Time',1,0,'C');
+            $this->Ln();
+        }
+        function viewTable($db){
+            $no = 1;
+            $deliveryfee = 3;
+            $totaldeliveryfee = 0;
+            $this->SetFont('Times', '', 12);
+            $this->SetXY(50,70);
+            foreach($db as $row){
+                $this->SetX(35);
+                $this->Cell(30,10,"$no",1,0,'C');
+                $this->Cell(50,10,$row['itemname'],1,0,'C');
+                $this->Cell(50,10,$row['itemquantity'],1,0,'C');
+                $this->Cell(50,10,"$deliveryfee",1,0,'C');
+                $this->Cell(50,10,$row['time'],1,0,'C');
+                $this->Ln();
+                $no++;
+                $totaldeliveryfee+=$deliveryfee;
+            }
+            $this->SetFont('Times', 'B', 12);
+            $this->SetX(35);
+            $this->Cell(30,20,'',0,0,'C');
+            $this->Cell(50,20,'',0,0,'C');
+            $this->Cell(50,20,'Total Commission',0,0,'C');
+            $this->Cell(50,20,"RM $totaldeliveryfee",0,0,'C');
+        }
+    }
+
+    if(isset($_POST['print'])){
+        $pdf = new printPDF();
+        $pdf->AliasNbPages();
+        $pdf->AddPage('L','A4',0);
+        $pdf->headerTable();
+        $pdf->body();
+        $pdf->viewTable($data);
+        $pdf->Output();
+    }
+        
+    
 
 ?>
 <!DOCTYPE html>
@@ -93,6 +174,14 @@
 
                 </table>
             </div>
+
+            <div style="margin-left: 1em; margin-top: 3em; text-decoration: underline;" >
+            <form action="" method="POST">
+                <button name="print" type="submit"><img src="Image/printer.png" alt="" width="40px" height="40px"></button>
+            </form>
+                
+            </div>
+            
         </center>
     </body>
 </html>
